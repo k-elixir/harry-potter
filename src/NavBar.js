3d09802ba5660b2063cb5ca9bf8ea46b4eb8aef8
Kimia <kimia.rfp@gmail.com>
2023-05-09 12:17:21 +0330

    create react app
    create react app
    installed bootstrap and add navbar in other component

---
+++ src/NavBar.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img src="./images/logo.png" alt="Logo" />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse  justify-content-end"
+          id="navbarNavDropdown"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Features
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Pricing
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="/"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown link
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="/">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
---
+++ src/homePage/FirstPart.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FirstPart() {
+  return <div className="HomePage-FirstPart"></div>;
+}
